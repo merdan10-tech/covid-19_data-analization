31ac8c3ad6593063c1da0e1b882a9db955e299b0
Merdan Garlyyev <118038545+merdan10-tech@users.noreply.github.com>
2024-07-06 21:30:52 -0500

    Update COVID_Portfolio_Project1.sql

--- COVID_Portfolio_Project1.sql
+++ COVID_Portfolio_Project1.sql
@@ -1,27 +1,27 @@
--- First dataset check
+-- Our first dataset check
 SELECT *
-FROM DA_Project1_Covid_Cases..CovidDeaths
+FROM PortfolioProjects..CovidDeaths
 ORDER BY 3,4 --this sorts by 3rd and 4th columns (location&date)
 
 
 -- Examine Total cases vs. Total Deaths
 -- Calculate the death percentage for each country and date it
 SELECT location, date, total_cases, total_deaths, round((total_deaths/total_cases) * 100, 2) AS death_percentage
-FROM DA_Project1_Covid_Cases..CovidDeaths
+FROM PortfolioProjects..CovidDeaths
 ORDER BY 1,2
 
 
 -- Examine Total Cases vs Population
 -- Depict the percentage of population infected by COVID-19
 SELECT location, date, population, total_cases, ROUND((total_cases/population) * 100, 6) AS case_ratio
-FROM DA_Project1_Covid_Cases..CovidDeaths
+FROM PortfolioProjects..CovidDeaths
 --WHERE location LIKE '%states%'
 ORDER BY 1,2
 
 
 -- Demonstrate Countries with the Highest Death Count per Population
 SELECT location, MAX(cast(total_deaths AS int)) AS ultimate_total_deaths
-FROM DA_Project1_Covid_Cases..CovidDeaths
+FROM PortfolioProjects..CovidDeaths
 WHERE continent IS NOT NULL
 GROUP BY location
 ORDER BY ultimate_total_deaths DESC
@@ -29,7 +29,7 @@ ORDER BY ultimate_total_deaths DESC
 
 -- Total Death Number by Continent
 SELECT continent, MAX(cast(total_deaths AS int)) AS ultimate_total_deaths
-FROM DA_Project1_Covid_Cases..CovidDeaths
+FROM PortfolioProjects..CovidDeaths
 WHERE continent IS NOT NULL
 GROUP BY continent
 ORDER BY ultimate_total_deaths DESC
@@ -37,7 +37,7 @@ ORDER BY ultimate_total_deaths DESC
 
 -- Calculate the Global Death Percentage Per Day
 SELECT date, SUM(new_cases) AS global_cases, SUM(cast(new_deaths as int)) AS global_deaths, SUM(cast(new_deaths as int))/SUM(new_cases)*100 AS global_death_ratio
-FROM DA_Project1_Covid_Cases..CovidDeaths
+FROM PortfolioProjects..CovidDeaths
 WHERE continent IS NOT NULL
 GROUP BY date
 ORDER BY 1,2
@@ -47,7 +47,7 @@ ORDER BY 1,2
 
 --Total Death Percentage All over the World
 SELECT SUM(new_cases) AS global_cases, SUM(cast(new_deaths as int)) AS global_deaths, SUM(cast(new_deaths as int))/SUM(new_cases)*100 AS global_death_percentage
-FROM DA_Project1_Covid_Cases..CovidDeaths
+FROM PortfolioProjects..CovidDeaths
 WHERE continent IS NOT NULL
 ORDER BY 1,2
 
@@ -57,7 +57,7 @@ ORDER BY 1,2
 -- Calculate the total number of deaths for locations without a specified continent, 
 -- excluding aggregate entities like 'World', 'European Union', and 'International'
 SELECT location, SUM(cast(new_deaths as int)) AS total_death_count
-FROM DA_Project1_Covid_Cases..CovidDeaths
+FROM PortfolioProjects..CovidDeaths
 WHERE continent IS NULL AND location NOT IN ('World', 'European Union', 'International')
 GROUP BY location
 ORDER BY total_death_count DESC
@@ -67,7 +67,7 @@ ORDER BY total_death_count DESC
 
 -- Display countries with the highest infection rate as a percentage of their population
 SELECT location, population, MAX(total_cases) AS ultimate_total_cases, ROUND(MAX((total_cases/population))* 100, 6) AS percent_of_infected
-FROM DA_Project1_Covid_Cases..CovidDeaths
+FROM PortfolioProjects..CovidDeaths
 GROUP BY location, population
 ORDER BY percent_of_infected DESC
 
@@ -76,7 +76,7 @@ ORDER BY percent_of_infected DESC
 
 -- Calculate the highest infection rate as a percentage of population for each location and date
 SELECT location, population, date, MAX(total_cases) AS ultimate_total_cases, ROUND(MAX((total_cases/population))* 100, 6) AS percent_of_infected
-FROM DA_Project1_Covid_Cases..CovidDeaths
+FROM PortfolioProjects..CovidDeaths
 GROUP BY location, population, date
 ORDER BY percent_of_infected DESC
 
@@ -87,8 +87,8 @@ ORDER BY percent_of_infected DESC
 -- Inspect Vaccination being used by amount of People
 SELECT d.continent, d.location, d.date, d.population, v.new_vaccinations
 , SUM(CONVERT (int, v.new_vaccinations)) OVER (PARTITION BY d.location ORDER BY d.location, d.date) AS rolling_people_vaccinated
-FROM DA_Project1_Covid_Cases..CovidDeaths AS d
-JOIN DA_Project1_Covid_Cases..CovidVaccinations AS v
+FROM PortfolioProjects..CovidDeaths AS d
+JOIN PortfolioProjects..CovidVaccinations AS v
 ON d.location = v.location AND d.date = v.date 
 WHERE d.continent IS NOT NULL
 ORDER BY 2,3
@@ -100,8 +100,8 @@ AS
 (
 SELECT d.continent, d.location, d.date, d.population, v.new_vaccinations
 , SUM(CONVERT (int, v.new_vaccinations)) OVER (PARTITION BY d.location ORDER BY d.location, d.date) AS rolling_people_vaccinated
-FROM DA_Project1_Covid_Cases..CovidDeaths AS d
-JOIN DA_Project1_Covid_Cases..CovidVaccinations AS v
+FROM PortfolioProjects..CovidDeaths AS d
+JOIN PortfolioProjects..CovidVaccinations AS v
 ON d.location = v.location AND d.date = v.date 
 WHERE d.continent IS NOT NULL
 )
@@ -126,8 +126,8 @@ INSERT INTO #PercentPopulationVaccinated
 
 SELECT d.continent, d.location, d.date, d.population, v.new_vaccinations
 , SUM(CONVERT (int, v.new_vaccinations)) OVER (PARTITION BY d.location ORDER BY d.location, d.date) AS rolling_people_vaccinated
-FROM DA_Project1_Covid_Cases..CovidDeaths AS d
-JOIN DA_Project1_Covid_Cases..CovidVaccinations AS v
+FROM PortfolioProjects..CovidDeaths AS d
+JOIN PortfolioProjects..CovidVaccinations AS v
 ON d.location = v.location AND d.date = v.date 
 
 SELECT *, (rolling_people_vaccinated/population) * 100 AS percentage_of_vaccinated
@@ -138,8 +138,8 @@ FROM #PercentPopulationVaccinated
 CREATE VIEW PercentPopulationVaccinated  AS 
 SELECT d.continent, d.location, d.date, d.population, v.new_vaccinations
 , SUM(CONVERT (int, v.new_vaccinations)) OVER (PARTITION BY d.location ORDER BY d.location, d.date) AS rolling_people_vaccinated
-FROM DA_Project1_Covid_Cases..CovidDeaths AS d
-JOIN DA_Project1_Covid_Cases..CovidVaccinations AS v
+FROM PortfolioProjects..CovidDeaths AS d
+JOIN PortfolioProjects..CovidVaccinations AS v
 ON d.location = v.location AND d.date = v.date 
 WHERE d.continent IS  NOT NULL
 --order by 2,3
@@ -164,7 +164,7 @@ CREATE TABLE #DeathPercentageByCountry (
 
 INSERT INTO #DeathPercentageByCountry
 SELECT location, date, total_cases, total_deaths, round((total_deaths/total_cases) * 100, 2) AS death_percentage
-FROM DA_Project1_Covid_Cases..CovidDeaths
+FROM PortfolioProjects..CovidDeaths
 ORDER BY 1,2
 
 --Separate the batch with GO
@@ -174,7 +174,7 @@ GO
 --Create a Table View 2
 CREATE VIEW DeathPercentageByCountry  AS 
 SELECT location, date, total_cases, total_deaths, round((total_deaths/total_cases) * 100, 2) AS death_percentage
-FROM DA_Project1_Covid_Cases..CovidDeaths
+FROM PortfolioProjects..CovidDeaths
 --ORDER BY 1,2
 
 
@@ -196,7 +196,7 @@ CREATE TABLE #PercentOfInfectedByCountry (
 
 INSERT INTO #PercentOfInfectedByCountry
 SELECT location, population, MAX(total_cases) AS ultimate_total_cases, ROUND(MAX((total_cases/population))* 100, 6) AS percent_of_infected
-FROM DA_Project1_Covid_Cases..CovidDeaths
+FROM PortfolioProjects..CovidDeaths
 GROUP BY location, population
 ORDER BY percent_of_infected DESC
 
@@ -206,7 +206,7 @@ GO
 --Create a Table View 3
 CREATE VIEW PercentOfInfectedByCountry  AS 
 SELECT location, population, MAX(total_cases) AS ultimate_total_cases, ROUND(MAX((total_cases/population))* 100, 6) AS percent_of_infected
-FROM DA_Project1_Covid_Cases..CovidDeaths
+FROM PortfolioProjects..CovidDeaths
 GROUP BY location, population
 
 --Testing the View Table 3
@@ -226,7 +226,7 @@ CREATE TABLE #GlobalDeathRatio (
 
 INSERT INTO #GlobalDeathRatio
 SELECT SUM(new_cases) AS global_cases, SUM(cast(new_deaths as int)) AS global_deaths, SUM(cast(new_deaths as int))/SUM(new_cases)*100 AS global_death_ratio
-FROM DA_Project1_Covid_Cases..CovidDeaths
+FROM PortfolioProjects..CovidDeaths
 WHERE continent IS NOT NULL
 ORDER BY 1,2
 
@@ -236,7 +236,7 @@ GO
 --Create a Table View 4
 CREATE VIEW GlobalDeathRatio  AS 
 SELECT SUM(new_cases) AS global_cases, SUM(cast(new_deaths as int)) AS global_deaths, SUM(cast(new_deaths as int))/SUM(new_cases)*100 AS global_death_ratio
-FROM DA_Project1_Covid_Cases..CovidDeaths
+FROM PortfolioProjects..CovidDeaths
 WHERE continent IS NOT NULL
 
 --Testing the View Table 4
